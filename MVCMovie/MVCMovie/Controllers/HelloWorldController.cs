2afd0bfb972b108c43b3a0b70d0780d61eb5e3c0
Nelly Cheboi <nellycheboi12@augustana.edu>
2017-12-01 09:29:44 -0600

    :rocket: Started on ASP.Net Web Framework getting started guide

---
+++ MVCMovie/MVCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        /// <summary>
+        /// GET: /HelloWorld/
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// GET: /HellowWorld/Welcome
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string Welcome(string name, int ID = 1)
+        {
+            // HttpUtility.HTMLEncode protects script exploits
+            return HttpUtility.HtmlEncode($"Hello {name}, NumTimes is: {ID}");
+        }
+    }
+}
